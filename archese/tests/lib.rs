eb23f7470da063acce7a9ae7c8ee9abeda9cd592
Tom Marsh <ts.marsh@gmail.com>
2015-09-08 13:44:07 -0400

    A more complete test

--- archese/tests/lib.rs
+++ archese/tests/lib.rs
@@ -4,4 +4,7 @@ extern crate archese;
 fn it_parses_tokens() {
     assert_eq!(archese::tokenize(")"), [")"]);
     assert_eq!(archese::tokenize("("), ["("]);
+    let program = "(begin (define r 10) (* pi (* r r)))";
+    let expected = ["(", "begin", "(", "define", "r", "10", ")", "(", "*", "pi", "(", "*", "r", "r", ")", ")", ")"];
+    assert_eq!(archese::tokenize(program), expected);
 }
